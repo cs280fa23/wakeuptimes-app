5bfd3c99c1050b9cadd0afde7921867050adf0b6
Ali Madooei <alimadooei@gmail.com>
2023-08-29 10:04:07 -0400

    Use JavaScript for Manipulating the DOM

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+
 function calcWakeUpTimes() {
   const fallAsleepTime = new Date();
   fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
 
   const wakeUpTime = new Date(fallAsleepTime);
-  const wakeUpTimes = [];
+  wakeUpHoursDiv.innerHTML = "";
   for (let i = 1; i <= 6; i++) {
     wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
     const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
       timeStyle: "short",
     });
-    wakeUpTimes.push(wakeUpTimeString);
+    const cycleDiv = document.createElement("div");
+    cycleDiv.classList.add("cycle");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = wakeUpTimeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
   }
-
-  console.log("Wake-up times:", wakeUpTimes.join(", "));
 }
 
 const calcBtn = document.getElementById("calc-btn");
